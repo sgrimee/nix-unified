623b1264e792b144bdbf001158fa8fdd121b56fd
sgrimee <sgrimee@gmail.com.me>
2023-08-29 10:51:46 +0200

    change starship config and fonts

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,18 +1,14 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   fonts = {
     fontDir.enable = true; # DANGER
     fonts = [
+      pkgs.meslo-lgs-nf
       (pkgs.nerdfonts.override {
         fonts = [
           "FiraCode"
           "FiraMono"
         ];
       })
-      pkgs.dejavu_fonts # mind the underscore! most of the packages are named with a hypen, not this one however
-      pkgs.meslo-lgs-nf
-      pkgs.noto-fonts
-      pkgs.noto-fonts-cjk
-      pkgs.noto-fonts-emoji
     ];
   };
 }
