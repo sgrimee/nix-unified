0cb12489fc93575a2397da953f4c36bb3b295cf3
sgrimee <sgrimee@gmail.com.me>
2023-09-01 15:45:08 +0200

    dock: only show running apps

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -10,9 +10,11 @@
     orientation = "bottom";
     show-recents = false;
     tilesize = 42; # dock icon size
+    static-only = true; # only show open apps
   };
 
   # local.dock options are defined in the ./dock module
   local.dock.enable = true;
+
   local.dock.entries = import ./dock-entries.nix {};
 }
