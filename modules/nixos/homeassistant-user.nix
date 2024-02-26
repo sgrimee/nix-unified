2ea355481f9d6d68f905a56c894b8f04b9339cb3
sgrimee <sgrimee@gmail.com.me>
2024-02-26 15:57:35 +0100

    fix: permissions for legion homeassistant suspend

--- modules/nixos/homeassistant-user.nix
+++ modules/nixos/homeassistant-user.nix
@@ -1,6 +1,25 @@
-{}:
-{
-  users.users.homeassistant.openssh.authorizedKeys.keys = [
-    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTwToeyPBUpY1zI0Mil9TxdpZJygx8W/0/+epVP/SIbJe+P4sBNKBoBLHJ4RnFJDhXsR0rM7/XABMw1BdDvH4AIC4CTOhOzYLda2Q+xJHO19yu63FR51nArophmbBRu5G8VdaKV4fAv4RdDFsKoar/Xee3FhwXPnhu8y0hQtVV5Kgw5nJlRPExzB392LfIrtc/e62dTbF+OD3VVnW7TKWOTJ9ngqqyashbvFONwdo6XrxW1+u9fqHQx5QXZx5N1c9F+2GB4EAZxUGOwCOTSX1CVpktjh3gi6kaHmdQ10oR+iv3vHwAbQtutDGPW6wCctZf49zu5+Z8ddh+eh+jDG8e4GczGKPyhtTE8vOTUtRpCuEMp45wmOT4aYiJWzoVRjsvoeR89Ym2CLdvProNjbKorP+7fcCdYSDDgI/DPoU2/F+a1ETxYqHKECoaphunXfHCwzjyPWSucCDKiseXee1kQKdy0aFlJI3xBKMMabR7YIe8UqWEpegFzzTJWX879hM= root@a0d7b954-ssh
-  ];
+{pkgs, ...}: {
+  users.groups.shutdown = {};
+  users.users.homeassistant = {
+    group = "shutdown";
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTwToeyPBUpY1zI0Mil9TxdpZJygx8W/0/+epVP/SIbJe+P4sBNKBoBLHJ4RnFJDhXsR0rM7/XABMw1BdDvH4AIC4CTOhOzYLda2Q+xJHO19yu63FR51nArophmbBRu5G8VdaKV4fAv4RdDFsKoar/Xee3FhwXPnhu8y0hQtVV5Kgw5nJlRPExzB392LfIrtc/e62dTbF+OD3VVnW7TKWOTJ9ngqqyashbvFONwdo6XrxW1+u9fqHQx5QXZx5N1c9F+2GB4EAZxUGOwCOTSX1CVpktjh3gi6kaHmdQ10oR+iv3vHwAbQtutDGPW6wCctZf49zu5+Z8ddh+eh+jDG8e4GczGKPyhtTE8vOTUtRpCuEMp45wmOT4aYiJWzoVRjsvoeR89Ym2CLdvProNjbKorP+7fcCdYSDDgI/DPoU2/F+a1ETxYqHKECoaphunXfHCwzjyPWSucCDKiseXee1kQKdy0aFlJI3xBKMMabR7YIe8UqWEpegFzzTJWX879hM= root@a0d7b954-ssh"
+    ];
+  };
+
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        commands = [
+          {
+            command = "/run/current-system/sw/bin/shutdown";
+            options = ["NOPASSWD"];
+          }
+        ];
+        groups = ["shutdown"];
+      }
+    ];
+  };
 }
