2a737fc9b5ba90b912fe7aa50649b67e166c1a60
sgrimee <sgrimee@gmail.com.me>
2024-02-26 18:32:57 +0100

    fix: ethtool not available on Darwin

--- modules/home-manager/user/packages.nix
+++ modules/home-manager/user/packages.nix
@@ -5,47 +5,50 @@
   unstable,
   ...
 }: {
-  home.packages = with pkgs; [
-    # common packages, found in ~/.nix-profile/bin
-    age
-    alejandra
-    coreutils-full
-    curl
-    du-dust
-    ethtool
-    glow # CLI markdown viewer
-    hamlib_4
-    home-manager
-    htop
-    inetutils
-    joshuto
-    killall
-    less
-    inputs.mactelnet.packages.${system}.mactelnet
-    mc
-    mitmproxy
-    neofetch
-    nixpkgs-fmt
-    openssh
-    pat
-    podman
-    progress
-    qemu
-    ripgrep
-    rnix-lsp
-    rustscan
-    # rustup
-    sops
-    spotify-tui
-    spotifyd
-    ssh-to-age
-    trippy
-    unzip
-    inputs.unstable.legacyPackages.${system}.vscode-langservers-extracted
-    wakeonlan
-    wget
-    yt-dlp
-    zellij
-    zip
-  ];
+  home.packages = with pkgs;
+    [
+      # common packages, found in ~/.nix-profile/bin
+      age
+      alejandra
+      coreutils-full
+      curl
+      du-dust
+      glow # CLI markdown viewer
+      hamlib_4
+      home-manager
+      htop
+      inetutils
+      joshuto
+      killall
+      less
+      inputs.mactelnet.packages.${system}.mactelnet
+      mc
+      mitmproxy
+      neofetch
+      nixpkgs-fmt
+      openssh
+      pat
+      podman
+      progress
+      qemu
+      ripgrep
+      rnix-lsp
+      rustscan
+      # rustup
+      sops
+      spotify-tui
+      spotifyd
+      ssh-to-age
+      trippy
+      unzip
+      inputs.unstable.legacyPackages.${system}.vscode-langservers-extracted
+      wakeonlan
+      wget
+      yt-dlp
+      zellij
+      zip
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      ethtool
+    ];
 }
