f5aceba312a8a3dc5c2769846f21575aacde452a
sgrimee <sgrimee@gmail.com.me>
2024-02-26 14:55:11 +0100

    feat: legion wake on lan

--- modules/hosts/legion/hardware-configuration.nix
+++ modules/hosts/legion/hardware-configuration.nix
@@ -41,6 +41,7 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp3s0.wakeOnLan.enable = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
