9887686b9ffb5d23d53ee5d16f8ba8728aa4ca34
sgrimee <sgrimee@gmail.com.me>
2024-06-19 11:22:40 +0200

    feat: hide direnv env diff again via config

--- modules/home-manager/user/programs/direnv.nix
+++ modules/home-manager/user/programs/direnv.nix
@@ -6,11 +6,8 @@
     nix-direnv.enable = true;
 
     config.global = {
+      hide_env_diff = true;
       warn_timeout = "1m";
     };
   };
-  # hide env variable diffs of direnv
-  programs.zsh.envExtra = ''
-    export DIRENV_HIDE_DIFF=1;
-  '';
 }
