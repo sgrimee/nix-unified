86f11eeeab29858311a860d8e4b3df7f9cad956b
sgrimee <sgrimee@gmail.com.me>
2024-07-17 14:20:54 +0200

    fix: remove outdated macbook air option for nixair

--- modules/hosts/nixair/default.nix
+++ modules/hosts/nixair/default.nix
@@ -11,8 +11,6 @@ in [
   # quirks
   # https://everymac.com/systems/apple/macbook-air/specs/macbook-air-core-i7-1.8-11-mid-2011-specs.html
   nixos-hardware.nixosModules.apple-macbook-air-4
-  nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
-  #nixos-hardware.nixosModules.common-hidpi  # make font too big on console
   nixos-hardware.nixosModules.common-pc-ssd
 
   # system
