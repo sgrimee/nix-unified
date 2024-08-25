6e66987b83641a43bbc759c37fcfea0fbcc6805f
sgrimee <sgrimee@gmail.com.me>
2024-08-25 17:50:37 +0200

    feat: manage /etc/hosts on darwin

--- modules/darwin/networking.nix
+++ modules/darwin/networking.nix
@@ -1,6 +1,23 @@
 {
   system.defaults.alf.globalstate = 1; # enable firewall
 
+  environment.etc."hosts".text = ''
+    ##
+    # managed by nix
+
+    # Host Database
+    #
+    # localhost is used to configure the loopback interface
+    # when the system is booting.  Do not change this entry.
+    ##
+    127.0.0.1	localhost
+    255.255.255.255	broadcasthost
+    ::1             localhost
+
+    10.0.1.186  samael
+    10.1.1.18   carnas
+  '';
+
   # networking = {
   #   # AdGuard DNS
   #   dns = [
