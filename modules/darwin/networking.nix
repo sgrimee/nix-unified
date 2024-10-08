860c24e8eaa8c0a92698a3b9ac3e296a70145248
sgrimee <sgrimee@gmail.com.me>
2024-10-08 13:40:39 +0200

    disable /etc/hosts management on mac due to vpn client

--- modules/darwin/networking.nix
+++ modules/darwin/networking.nix
@@ -1,30 +1,21 @@
 {
   system.defaults.alf.globalstate = 1; # enable firewall
 
-  environment.etc."hosts".text = ''
-    ##
-    # managed by nix
+  # Disabling because the /etc/hosts file is edited by VpnClient
+  # environment.etc."hosts".text = ''
+  #   ##
+  #   # managed by nix
 
-    # Host Database
-    #
-    # localhost is used to configure the loopback interface
-    # when the system is booting.  Do not change this entry.
-    ##
-    127.0.0.1	localhost
-    255.255.255.255	broadcasthost
-    ::1             localhost
+  #   # Host Database
+  #   #
+  #   # localhost is used to configure the loopback interface
+  #   # when the system is booting.  Do not change this entry.
+  #   ##
+  #   127.0.0.1	localhost
+  #   255.255.255.255	broadcasthost
+  #   ::1             localhost
 
-    10.0.1.186  samael
-    10.1.1.18   carnas
-  '';
-
-  # networking = {
-  #   # AdGuard DNS
-  #   dns = [
-  #     "94.140.14.14"
-  #     "94.140.15.15"
-  #   ];
-
-  #   knownNetworkServices = [ "Wi-Fi" ];
-  # };
+  #   10.0.1.186  samael
+  #   10.1.1.18   carnas
+  # '';
 }
