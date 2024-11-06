2199aa8a9160999d8c6a4b6e3f93e33f20a4069e
sgrimee <sgrimee@gmail.com.me>
2024-11-06 10:30:01 +0100

    nixup & remove ovsolete login shell

--- modules/darwin/environment.nix
+++ modules/darwin/environment.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   environment = {
     shells = [pkgs.zsh];
-    loginShell = pkgs.zsh;
     # darwin only system packages, go to /run/current-system/sw
     systemPackages = with pkgs; [
       dockutil
