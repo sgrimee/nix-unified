511f449320b77654e4de1b28f75c854066e76d0f
sgrimee <sgrimee@gmail.com.me>
2024-12-16 14:30:39 +0100

    fix: sound on nixos

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -1,17 +1,17 @@
 {
   # Enable sound with pipewire.
   nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio.enable = false;
+  # hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   jack.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
 
-  #   # use the example session manager (no others are packaged yet so this is enabled by default,
-  #   # no need to redefine it in your config for now)
-  #   #media-session.enable = true;
-  # };
+    #   # use the example session manager (no others are packaged yet so this is enabled by default,
+    #   # no need to redefine it in your config for now)
+    #   #media-session.enable = true;
+  };
 }
