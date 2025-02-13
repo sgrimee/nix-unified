e06b5da3d034b164e7ff20936b0a977eafa31ac6
sgrimee <sgrimee@gmail.com.me>
2025-02-13 16:43:21 +0100

    remove duplicate source

--- modules/home-manager/user/programs/carapace.nix
+++ modules/home-manager/user/programs/carapace.nix
@@ -10,6 +10,5 @@
     export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
     zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
     zstyle ':completion:*' menu select
-    source <(carapace _carapace)
   '';
 }
