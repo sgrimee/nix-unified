dc8d14e5b2433409bce69267b7a022c39b957b55
sgrimee <sgrimee@gmail.com.me>
2025-04-21 13:14:47 +0200

    fix nvidia config

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -6,8 +6,7 @@
 }: {
   hardware.opengl = {
     enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+    # driSupport32Bit = true;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
