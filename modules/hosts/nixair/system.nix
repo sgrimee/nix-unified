0558d95c0b997cd348f2eff780ebe2caed4271f4
Sam Grimee <sgrimee@gmail.com>
2025-04-21 19:18:34 +0200

    dist build for nixair

--- modules/hosts/nixair/system.nix
+++ modules/hosts/nixair/system.nix
@@ -11,4 +11,17 @@
     max-jobs = 2; # Number of parallel build processes
     cores = 2; # Threads per build (see note below)
   };
+
+  nix = {
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "legion.local";
+        sshUser = "sgrimee";
+        sshKey = "/home/sgrimee/.ssh/id_rsa";
+        system = "x86_64-linux";
+        supportedFeatures = ["kvm" "nixos-test" "big-parallel"];
+      }
+    ];
+  };
 }
