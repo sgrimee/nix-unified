fd84dc51cf9bb18ee311b5da822d4b5f898cbf78
sgrimee <sgrimee@gmail.com.me>
2025-09-16 21:35:50 +0200

    fix pam setup

--- modules/darwin/sudo.nix
+++ modules/darwin/sudo.nix
@@ -4,4 +4,7 @@
   environment.etc."sudoers.d/timeout".text = ''
     Defaults timestamp_timeout=15
   '';
+
+  # Authentication methods for sudo
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,13 +1,8 @@
 {
-  # Add ability to used TouchID for sudo authentication
-  security.pam.services.sudo_local.touchIdAuth = true;
-
   system = {
     primaryUser = "sgrimee";
     defaults = {
-      LaunchServices = {
-        LSQuarantine = false;
-      };
+      LaunchServices = { LSQuarantine = false; };
       loginwindow = {
         DisableConsoleAccess = true;
         GuestEnabled = false;
@@ -19,16 +14,16 @@
         ShowDayOfMonth = true;
       };
       NSGlobalDomain = {
-        AppleInterfaceStyleSwitchesAutomatically = true; # auto dark / light mode
+        AppleInterfaceStyleSwitchesAutomatically =
+          true; # auto dark / light mode
         AppleShowAllExtensions = true;
         ApplePressAndHoldEnabled = false;
         "com.apple.mouse.tapBehavior" = 1;
         "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0; # disable sound when changing volume
+        "com.apple.sound.beep.feedback" =
+          0; # disable sound when changing volume
       };
-      screencapture = {
-        location = "~/Downloads";
-      };
+      screencapture = { location = "~/Downloads"; };
     };
   };
 }
