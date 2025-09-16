0bafcbfe279cc6595f163699f4057ec42518040b
sgrimee <sgrimee@gmail.com.me>
2025-09-16 12:19:55 +0200

    fix distributed builds in determinate

--- hosts/darwin/SGRIMEE-M-4HJT/system.nix
+++ hosts/darwin/SGRIMEE-M-4HJT/system.nix
@@ -12,7 +12,7 @@
   # Host-specific Determinate Nix settings
   determinate-nix.customSettings = {
     # Distributed builds configuration
-    distributed-builds = true;
+    builders-use-substitutes = true;
     builders =
       "ssh://sgrimee@cirice.local x86_64-linux /Users/sgrimee/.ssh/id_ed25519 8 100 kvm,nixos-test,big-parallel c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUxsMUt4UEpXWFdHNnZOTTBEMVJkRllOWGx6UnNtaEVvclhjaVV4cER6M3ggcm9vdEBjaXJpY2UK ssh://sgrimee@legion.local x86_64-linux /Users/sgrimee/.ssh/id_ed25519 8 50 kvm,nixos-test,big-parallel c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBydlBka3VmUFNoMEFJWi9mSWh0V25rd3NqV2x6anQ5RWpEK2J5bDY4Z0sgcm9vdEBuaXhvcwo=";
   };
