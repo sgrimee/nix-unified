e5bc1e7fdd573f0c12d8496310066cf2e58d05c1
sgrimee <sgrimee@gmail.com.me>
2025-10-02 18:09:29 +0200

    simpligy spotifyd config

--- hosts/nixos/cirice/spotifyd.nix
+++ hosts/nixos/cirice/spotifyd.nix
@@ -3,11 +3,7 @@
     enable = true;
     settings = {
       global = {
-        username_cmd = "echo $(</run/secrets/spotify_userid)";
-        password_cmd = "echo $(</run/secrets/spotify_secret)";
         device_name = "nixair";
-        zeroconf_port = 5354;
-        backend = "pulseaudio";
         bitrate = 320;
       };
     };
