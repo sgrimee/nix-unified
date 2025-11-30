78791ea9c94106ba5613b3314fd37a8379c66100
sgrimee <sgrimee@gmail.com.me>
2025-11-30 16:48:29 +0100

    Fix fish deroff.py issue by disabling completion generation
    Fish 4.2.1 removed deroff.py but completion generator still expected it,
    causing build failures. Added generateCompletions = false to both:
    - User fish config (modules/home-manager/user/programs/shells/fish.nix)
    - Main fish config (modules/home-manager/default.nix)
    
    This prevents fish_patched-completion-generator from running and avoids
    the missing deroff.py error. Build now succeeds with fewer derivations.

--- modules/home-manager/user/programs/shells/fish.nix
+++ modules/home-manager/user/programs/shells/fish.nix
@@ -7,5 +7,8 @@
     enable =
       true; # installed as a package from unstable to get the rust version
     package = inputs.unstable.legacyPackages.${system}.fish;
+
+    # Disable automatic completion generation to avoid deroff.py issues
+    generateCompletions = false;
   };
 }
