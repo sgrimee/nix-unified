03ec55bbf7e6fd786d31749cdb5c73165023fde0
sgrimee <sgrimee@gmail.com.me>
2025-11-30 17:22:43 +0100

    add printers to nixos

--- modules/nixos/printing.nix
+++ modules/nixos/printing.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   services.printing = {
     enable = true;
-    drivers = [pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper];
+    drivers = [pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper pkgs.mfcl3770cdwcupswrapper pkgs.gutenprint];
   };
+
+  environment.systemPackages = with pkgs; [
+    mfcl3770cdwcupswrapper
+  ];
 }
