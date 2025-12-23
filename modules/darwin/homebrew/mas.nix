d6471f4b6f3928e5c29549ab4b23596afcf93d56
sgrimee <sgrimee@gmail.com.me>
2025-12-23 12:02:09 +0100

    remove mas apps

--- modules/darwin/homebrew/mas.nix
+++ modules/darwin/homebrew/mas.nix
@@ -7,12 +7,12 @@
   # Productivity & Organization
   "MindNode" = 1289197285; # Mind mapping and brainstorming tool
   "Overlap" = 1516950324; # Time zone converter and meeting planner
-  "Paprika Recipe Manager 3" = 1303222628; # Recipe management and meal planning
+  # "Paprika Recipe Manager 3" = 1303222628; # Recipe management and meal planning
 
   # System & Network Utilities
   "Discovery" = 1381004916; # Network discovery and port scanning
   "Speedtest" = 1153157709; # Internet speed testing by Ookla
-  "Plash" = 1494023538; # Website as desktop wallpaper
+  # "Plash" = 1494023538; # Website as desktop wallpaper
 
   # Ham Radio & SDR
   "SDR Control for Icom" = 1592313285; # Software defined radio control for Icom
@@ -21,5 +21,5 @@
   # Specialized Tools
   "eidReader" = 1190651975; # Belgian eID card reader
   "Brother P-touch Editor" = 1453365242; # Brother label printer editor
-  "MacFamilyTree 10" = 1567970985; # Genealogy and family tree software
+  # "MacFamilyTree 10" = 1567970985; # Genealogy and family tree software
 }
