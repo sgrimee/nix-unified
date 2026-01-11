c5f0e38f5261fe24b32f240cba7a625f88b814e3
sgrimee <sgrimee@gmail.com.me>
2026-01-11 14:57:09 +0100

    update helix config

--- modules/home-manager/user/programs/helix.nix
+++ modules/home-manager/user/programs/helix.nix
@@ -15,12 +15,9 @@
       };
       keys.normal = {
         esc = ["collapse_selection" "keep_primary_selection"];
-        o = "file_picker_in_current_buffer_directory";
-        #p = "paste_clipboard_before";
         space.q = ":q";
         space.space = "file_picker";
         space.w = ":w";
-        #y = "yank_main_selection_to_clipboard";
       };
     };
     languages = {
